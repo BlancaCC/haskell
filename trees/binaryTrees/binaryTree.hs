1dbae31014152e03c0571be46e338a431a42be0b
Blanca <blancacano@correo.ugr.es>
2019-11-03 10:07:54 +0100

    [trees/binaryTree] first approach to binary tree, a funcional (in both meaning xD)

---
+++ trees/binaryTrees/binaryTree.hs
@@ -0,0 +1,83 @@
+{-
+Binary trees in haskell
+Funtions:
+- Definition
+- isBinT (if an element is in a bt
+- listToBinTree
+- drawBinTree
+
+
+Example:
+*Main> drawBinTree (listToBinTree [1..10]) 0
+|
+|-1
+  |
+  |-2
+    |
+    |-4
+      |
+      |-8
+    |
+    |-6
+      |
+      |-10
+  |
+  |-3
+    |
+    |-5
+      |
+      |-9
+    |
+    |-7
+
+Date: Granda 3/X/19
+-}
+
+
+--- Definition ---
+data BinTree t = VoidB | Node t (BinTree t) (BinTree t) deriving Show
+
+
+-- declaration of a Bin tree 
+bt :: BinTree Integer
+bt = Node 0 (Node 1 VoidB VoidB) VoidB
+
+
+---- other function -----
+
+-- determinate if a is in a BinTree
+
+isInBinT:: Eq a => a -> BinTree a -> Bool
+isInBinT a VoidB = False
+isInBinT a (Node n l r)
+  | a == n = True
+  | otherwise = (isInBinT a l) || (isInBinT a r)
+
+
+
+-- part a list
+partList :: [a] -> [a]
+partList [] = []
+partList (x:[])= x:[]
+partList (x:_:[]) = [x]
+partList (x:_:xs) = x:(partList xs)
+
+
+-- create a BinTree from a list
+listToBinTree :: [a] -> BinTree a
+listToBinTree [] = VoidB
+listToBinTree (x:xs) = Node x (listToBinTree (partList xs)) (listToBinTree (partList (drop 1 xs)))
+
+
+  
+-- draw Tree
+space rep = take (2*rep) (repeat ' ')
+  
+drawBinTree VoidB _ = putStr ""
+drawBinTree (Node n l r) rep =do
+  putStrLn((space rep) <> "|")
+  putStrLn ((space rep) <>"|-"<>(show n))
+
+  drawBinTree l (rep+1)
+  drawBinTree r (rep+1)
+ 
