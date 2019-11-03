090c0648c9e843084d5cf3a79318e8e495ea64e8
Blanca <blancacano@correo.ugr.es>
2019-11-03 22:03:41 +0100

    [binaryTrees/binaryTree] divList new incredible implementation of partList <3

--- trees/binaryTrees/binaryTree.hs
+++ trees/binaryTrees/binaryTree.hs
@@ -8,7 +8,7 @@ Funtions:
 
 
 Example:
-*Main> drawBinTree (listToBinTree [1..6]) 0
+*Main> drawBinTree (listToBinTree [1..6])
 |
 |-1
   |
@@ -51,24 +51,42 @@ isInBinT a (Node n l r)
 -- create a BinTree from a list
 listToBinTree :: [a] -> BinTree a
 listToBinTree [] = VoidB
-listToBinTree (x:xs) = Node x (listToBinTree (partList xs)) (listToBinTree (partList (drop 1 xs)))
-  where partList :: [a] -> [a]
+listToBinTree (x:xs) = Node x (listToBinTree (r)) (listToBinTree (l))
+  where
+    (r,l) = divList xs
+    divList :: [a] -> ([a],[a])
+    divList [] = ([],[])
+    divList [x] = ([x],[])
+    divList (x:y:zs) = (x:xs, y:ys)
+      where
+        (xs,ys) = divList zs
+
+
+--old implementation of divList:
+    {-
+partList :: [a] -> [a]
         partList [] = []
         partList (x:[])= x:[]
         partList (x:_:[]) = [x]
         partList (x:_:xs) = x:(partList xs)
-
-
-  
+-}
 -- draw Tree
 space :: Int -> [Char]
 space rep = take (2*rep) (repeat ' ')
-  
-drawBinTree VoidB _ = putStr ""
-drawBinTree (Node n l r) rep =do
-  putStrLn((space rep) <> "|")
-  putStrLn ((space rep) <>"|-"<>(show n))
 
-  drawBinTree l (rep+1)
-  drawBinTree r (rep+1)
- 
+drawBinTree :: Show a => BinTree a -> IO()
+drawBinTree t = drawBinTreeAux t 0
+  where 
+    drawBinTreeAux :: Show a => BinTree a -> Int -> IO ()
+    drawBinTreeAux VoidB _ = putStr ""
+    drawBinTreeAux (Node n l r) rep = do
+      -- "beauty" index 
+      putStrLn((space rep) <> "|")
+      putStrLn ((space rep) <>"|-"<>(show n))
+
+      drawBinTreeAux l (rep+1)
+      drawBinTreeAux r (rep+1)
+    -- where  
+    space :: Int -> [Char]
+    space rep = take (2*rep) (repeat ' ')
+
